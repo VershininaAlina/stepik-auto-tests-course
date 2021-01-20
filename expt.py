b0c2614b0977dfd909beda218c2fb574f8538f85
VershininaAlina <Kirillastrov@gmail.com>
2021-01-21 02:09:58 +0700

    Это тест посвящен ожидаемым результатам

---
+++ expt.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+import os 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = WebDriverWait(browser, 10).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    button1 = browser.find_element_by_css_selector("#book")
+    button1.click()
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_css_selector("#answer")
+    input1.send_keys(y)
+    button2 = browser.find_element_by_css_selector("#solve")
+    button2.click()
+  
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit() 
